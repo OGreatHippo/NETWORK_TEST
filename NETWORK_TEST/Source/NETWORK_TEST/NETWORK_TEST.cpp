e41b41b81eb5383fc54b0d30951c21f393364ff8
Sean Nichols <78358189+OGreatHippo@users.noreply.github.com>
2023-05-21 16:27:20 +0100

    Project added

---
+++ NETWORK_TEST/Source/NETWORK_TEST/NETWORK_TEST.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NETWORK_TEST.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NETWORK_TEST, "NETWORK_TEST" );
---
+++ NETWORK_TEST/Source/NETWORK_TEST/NETWORK_TESTGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NETWORK_TESTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORK_TEST_API ANETWORK_TESTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
